13c274d8c8b41e5f647f08b39b416191e27ea5af
LvL090 <calvod879@gmail.com>
2022-02-22 13:04:42 +0100

    clearcode

--- accion.js
+++ accion.js
@@ -1,17 +1,17 @@
 /*--POPUP RON--*/
 const button = document.querySelector('#ron');
 const popup = document.querySelector('.popup-wrapper');
- 
+    
 button.addEventListener('click', () => {
     popup.style.display = 'block';
 });
 
 const close = document.querySelector('.popup-close');
- 
+    
 close.addEventListener('click', () => {
     popup.style.display = 'none';
 });
- 
+
 popup.addEventListener('click', e => {
     // console.log(e);
     if(e.target.className === 'popup-wrapper') {
