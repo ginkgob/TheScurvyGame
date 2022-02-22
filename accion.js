e7bd9f1013d2575acccd34c18820c3441e4f1292
LvL090 <calvod879@gmail.com>
2022-02-22 10:24:50 +0100

    add popup function

---
+++ accion.js
@@ -0,0 +1,24 @@
+/*--POPUP RON--*/
+const button = document.querySelector('#ron');
+const popup = document.querySelector('.popup-wrapper');
+ 
+button.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+
+const close = document.querySelector('.popup-close');
+ 
+button.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+ 
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+ 
+popup.addEventListener('click', e => {
+    // console.log(e);
+    if(e.target.className === 'popup-wrapper') {
+        popup.style.display = 'none';
+    }
+});
