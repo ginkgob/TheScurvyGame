93101c3ebac5bf0f971072e43af138bf86b084fe
ginkgo <alexvid33@gmail.com>
2022-02-23 14:56:44 +0100

    add mediaqueries and responsive

--- accion.js
+++ accion.js
@@ -2,21 +2,21 @@
 
 const button = document.querySelector('#ron');
 const popup = document.querySelector('.popup-wrapper');
- 
+
 button.addEventListener('click', () => {
     popup.style.display = 'block';
 });
 
 const close = document.querySelector('.popup-close');
- 
+
 button.addEventListener('click', () => {
     popup.style.display = 'block';
 });
- 
+
 close.addEventListener('click', () => {
     popup.style.display = 'none';
 });
- 
+
 popup.addEventListener('click', e => {
     // console.log(e);
     if(e.target.className === 'popup-wrapper') {
