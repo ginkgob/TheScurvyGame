6d18ad862f69f57236aa46a9f33a849e2171ebbf
ginkgo <alexvid33@gmail.com>
2022-02-24 11:18:36 +0100

    add history popup

--- accion.js
+++ accion.js
@@ -1,4 +1,4 @@
-/*--POPUP RON--*/
+//----POPUP RON-----
 
 const ronbutton = document.querySelector('#ron');
 const popup = document.querySelector('.popup-wrapper');
@@ -24,3 +24,21 @@ popup.addEventListener('click', e => {
     }
 });
 
+///-----POPUP HISTORY-----
+
+const paperbutton = document.querySelector('#paper');
+const child = document.querySelector('.parent');
+
+paperbutton.addEventListener('click', () => {
+    child.style.display = 'block';
+});
+
+const closing = document.querySelector('.close');
+
+paperbutton.addEventListener('click', () => {
+    child.style.display = 'block';
+});
+
+closing.addEventListener('click', () => {
+    child.style.display = 'none';
+});
