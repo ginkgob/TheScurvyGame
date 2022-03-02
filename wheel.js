18fef84382f628949a85cf008f286e9c3789529d
ginkgo <alexvid33@gmail.com>
2022-03-02 16:03:39 +0100

    wheel of doom

--- wheel.js
+++ wheel.js
@@ -10,8 +10,8 @@ function forTesting (array1, array2) {
         nameSubstraction = array1.splice(randomName, 1).toString(); //Quitamos ese nombre random de array1
         addToArray = array2.unshift(nameSubstraction); //Colocamos el nombre random en array2
         console.log(array1, array2);
-        document.querySelector('.playernames').innerHTML = array1;
-        document.querySelector('.deadPlayers').innerHTML = array2;
+        document.querySelector('#boxBl2').innerHTML = array1;
+        document.querySelector('#boxBr2').innerHTML = array2;
     }
     return result;
 };
