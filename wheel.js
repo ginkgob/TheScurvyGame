eabd28a75fc9fd4038b728b15dfb512b989b06a5
ginkgo <alexvid33@gmail.com>
2022-03-02 08:45:17 +0100

    fixed wheel of doom inside the console

--- wheel.js
+++ wheel.js
@@ -65,10 +65,10 @@ let result = chooseRandomPlayer(alivePlayers);
 
 function forTesting (array1, array2) {
     let result = 0;
-    if (array1.length>0) {
+    if (array1.length > 0) {
         randomName = Math.floor(Math.random()*array1.length); //Agarramos un nombre random de array1
         nameSubstraction = array1.splice(randomName, 1).toString(); //Quitamos ese nombre random de array1
-        addToArray = array2.push(nameSubstraction); //Colocamos el nombre random en array2
+        addToArray = array2.unshift(nameSubstraction[0]); //Colocamos el nombre random en array2
         console.log(array1, array2);
     }
     return result;
