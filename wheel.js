e4a65993458a1a19d823a81af2125b40725cee69
ginkgo <alexvid33@gmail.com>
2022-03-02 18:50:02 +0100

    deleted unnecessary comments

--- wheel.js
+++ wheel.js
@@ -66,9 +66,9 @@ let result = chooseRandomPlayer(alivePlayers);
 function killPlayers (array1, array2) {
     let result = 0;
     if (array1.length > 0) {
-        randomName = Math.floor(Math.random()*array1.length); //Agarramos un nombre random de array1
-        nameSubstraction = array1.splice(randomName, 1).toString(); //Quitamos ese nombre random de array1
-        addToArray = array2.push(nameSubstraction); //Colocamos el nombre random en array2
+        randomName = Math.floor(Math.random()*array1.length); 
+        nameSubstraction = array1.splice(randomName, 1).toString(); 
+        addToArray = array2.push(nameSubstraction); 
         console.log(array1, array2);
         document.querySelector('#alivePlayers').innerHTML = array1.join(" ");
         document.querySelector('#deadPlayers').innerHTML = array2.join(" ");
