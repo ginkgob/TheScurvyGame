661f4368e5e581c27e99214059cdfd873e6d4418
ginkgo <alexvid33@gmail.com>
2022-03-02 18:29:24 +0100

    put the chosen names in columns

--- wheel.js
+++ wheel.js
@@ -3,15 +3,15 @@
 let alivePlayers = ['Abde', 'Adrian', 'Albert', 'Ales', 'Alex', 'Daniel', 'Damaris', 'Guillermo', 'Ingryd', 'Jess', 'Liviu', 'Lucas', 'Miguel', 'Scarlet', 'Sergi', 'Uri'];
 let deadPlayers = [];
 
-function forTesting (array1, array2) {
+function killPlayers (array1, array2) {
     let result = 0;
     if (array1.length > 0) {
         randomName = Math.floor(Math.random()*array1.length); //Agarramos un nombre random de array1
         nameSubstraction = array1.splice(randomName, 1).toString(); //Quitamos ese nombre random de array1
-        addToArray = array2.unshift(nameSubstraction); //Colocamos el nombre random en array2
+        addToArray = array2.push(nameSubstraction); //Colocamos el nombre random en array2
         console.log(array1, array2);
-        document.querySelector('#boxBl2').innerHTML = array1;
-        document.querySelector('#boxBr2').innerHTML = array2;
+        document.querySelector('#alivePlayers').innerHTML = array1.join(" ");
+        document.querySelector('#deadPlayers').innerHTML = array2.join(" ");
     }
     return result;
 };
