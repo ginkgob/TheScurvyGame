4eacbdf1dc338c9e28f5c9536e4f20a6b9e9f145
ginkgo <alexvid33@gmail.com>
2022-03-03 18:53:25 +0100

    linked avatar with randomplayer

--- wheel.js
+++ wheel.js
@@ -1,31 +1,30 @@
 //-----WHEEL OF DOOM-----
 
-let alivePlayers = ['Abde', 'Adrian', 'Albert', 'Ales', 'Alex', 'Daniel', 'Damaris', 'Guillermo', 'Ingryd', 'Jess', 'Liviu', 'Lucas', 'Miguel', 'Scarlet', 'Sergi', 'Uri'];
+let alivePlayers = ['Abde', 'Adrian', 'Albert', 'Ales', 'Alex', 'Daniel', 'Damaris', 'Guillermo', 'Ingryd', 'Jess', 'Liviu', 'Lucas', 'Miguel', 'Scarlet', 'Sergi', 'Uri', 'Jose M.', 'Laura'];
 let deadPlayers = [];
 
 let avatarImg = document.getElementById('avatar');
 
-/* let imgs = {
+let imgs = {
     'Abde': './avatar png/Abde-removebg-preview.png',
-    'Adrian': './avatar png/Adrian-removebg-preview.png'
-} */
-
-let abde = document.getElementById('abde');
-let adrian = document.getElementById('adrian');
-let albert = document.getElementById('albert');
-let ales = document.getElementById('ales');
-let alex = document.getElementById('alex');
-let daniel = document.getElementById('daniel');
-let damaris = document.getElementById('damaris');
-let guillermo = document.getElementById('guillermo');
-let ingryd = document.getElementById('ingryd');
-let jess = document.getElementById('jess');
-let liviu = document.getElementById('liviu');
-let lucas = document.getElementById('lucas');
-let miguel = document.getElementById('miguel'); 
-let scarlet = document.getElementById('scarlet');
-let sergi = document.getElementById('sergi');
-let uri = document.getElementById('uri');
+    'Adrian': './avatar png/Adrian-removebg-preview.png',
+    'Albert': './avatar png/Albert-removebg-preview.png',
+    'Ales': './avatar png/Ales-removebg-preview.png',
+    'Alex': './avatar png/Alex-removebg-preview.png',
+    'Daniel': './avatar png/Daniel-removebg-preview.png',
+    'Damaris': './avatar png/Damaris.png',
+    'Guillermo': './avatar png/Guillermo-removebg-preview.png',
+    'Ingryd': './avatar png/Ingryd-removebg-preview.png',
+    'Jess': './avatar png/Jess-removebg-preview.png',
+    'Liviu': './avatar png/Liviu-removebg-preview.png',
+    'Lucas': './avatar png/Lucas-removebg-preview.png',
+    'Miguel': './avatar png/Miguel2.png',
+    'Scarlet': './avatar png/Scarlet.png',
+    'Sergi': './avatar png/Sergi-removebg-preview.png',
+    'Uri': './avatar png/Uri-removebg-preview.png',
+    'Jose M.': './avatar png/Jose.png',
+    'Laura': './avatar png/Laura.png'
+} 
 
 function killPlayers (array1, array2) {
     if (array1.length > 0) {
@@ -35,106 +34,109 @@ function killPlayers (array1, array2) {
             console.log(array1, array2);
         document.querySelector('#alivePlayers').innerHTML = array1.join(" ");
         document.querySelector('#deadPlayers').innerHTML = array2.join(" ");
-    };
-switch (nameSubstraction) {
-    case 'Abde': 
-        abde.style.display = 'block';
-        setTimeout(function() {
-            abde.style.display = 'none'
-        }, 2000);
-        break;
-    case 'Adrian': 
-        adrian.style.display = 'block';
-        setTimeout(function() {
-            adrian.style.display = 'none'
-        }, 2000); 
-        break;
-    case 'Albert':
-        albert.style.display = 'block';
-        setTimeout(function() {
-            albert.style.display = 'none'
-        }, 2000);
-        break;
-    case 'Ales': 
-        ales.style.display = 'block';
-        setTimeout(function() {
-            ales.style.display = 'none'
-        }, 2000);
-        break;
-    case 'Alex': 
-        alex.style.display = 'block';
-        setTimeout(function() {
-            alex.style.display = 'none'
-        }, 2000);
-        break;
-    case 'Damaris': 
-        damaris.style.display = 'block';
-        setTimeout(function() {
-        damaris.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Daniel': 
-        daniel.style.display = 'block';
-        setTimeout(function() {
-            daniel.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Guillermo':
-        guillermo.style.display = 'block';
-        setTimeout(function() {
-            guillermo.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Ingryd': 
-        ingryd.style.display = 'block';
-        setTimeout(function() {
-            ingryd.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Jess': 
-        jess.style.display = 'block';
-        setTimeout(function() {
-            jess.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Liviu': 
-        liviu.style.display = 'block';
-        setTimeout(function() {
-            liviu.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Lucas': 
-        lucas.style.display = 'block';
-        setTimeout(function() {
-            lucas.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Scarlet': 
-        scarlet.style.display = 'block';
-        setTimeout(function() {
-            scarlet.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Sergi': 
-        sergi.style.display = 'block';
-        setTimeout(function() {
-            sergi.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Uri': 
-        uri.style.display = 'block';
-        setTimeout(function() {
-            uri.style.display = 'none'
-        }, 2000);
-            break;
-    case 'Miguel': 
-        miguel.style.display = 'block';
-        setTimeout(function() {
-            miguel.style.display = 'none'
-        }, 2000);
-            break;
-    default: //no imprime ya que el if array1.lenght > 0, con >= quedan valores libres
-            console.log('¡¡¡FIN DE CICLO!!!');
-        };
+    }
+    let imgSrc = imgs[nameSubstraction] || './avatar png/Damaris.png';
+    avatarImg.src = imgSrc;
 };
 
+/*
+    switch (nameSubstraction) {
+        case 'Abde': 
+            abde.style.display = 'block';
+            setTimeout(function() {
+                abde.style.display = 'none'
+            }, 2000);
+            break;
+        case 'Adrian': 
+            adrian.style.display = 'block';
+            setTimeout(function() {
+                adrian.style.display = 'none'
+            }, 2000); 
+            break;
+        case 'Albert':
+            albert.style.display = 'block';
+            setTimeout(function() {
+                albert.style.display = 'none'
+            }, 2000);
+            break;
+        case 'Ales': 
+            ales.style.display = 'block';
+            setTimeout(function() {
+                ales.style.display = 'none'
+            }, 2000);
+            break;
+        case 'Alex': 
+            alex.style.display = 'block';
+            setTimeout(function() {
+                alex.style.display = 'none'
+            }, 2000);
+            break;
+        case 'Damaris': 
+            damaris.style.display = 'block';
+            setTimeout(function() {
+            damaris.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Daniel': 
+            daniel.style.display = 'block';
+            setTimeout(function() {
+                daniel.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Guillermo':
+            guillermo.style.display = 'block';
+            setTimeout(function() {
+                guillermo.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Ingryd': 
+            ingryd.style.display = 'block';
+            setTimeout(function() {
+                ingryd.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Jess': 
+            jess.style.display = 'block';
+            setTimeout(function() {
+                jess.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Liviu': 
+            liviu.style.display = 'block';
+            setTimeout(function() {
+                liviu.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Lucas': 
+            lucas.style.display = 'block';
+            setTimeout(function() {
+                lucas.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Scarlet': 
+            scarlet.style.display = 'block';
+            setTimeout(function() {
+                scarlet.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Sergi': 
+            sergi.style.display = 'block';
+            setTimeout(function() {
+                sergi.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Uri': 
+            uri.style.display = 'block';
+            setTimeout(function() {
+                uri.style.display = 'none'
+            }, 2000);
+                break;
+        case 'Miguel': 
+            miguel.style.display = 'block';
+            setTimeout(function() {
+                miguel.style.display = 'none'
+            }, 2000);
+                break;
+        default: //no imprime ya que el if array1.lenght > 0, con >= quedan valores libres
+                console.log('¡¡¡FIN DE CICLO!!!');
+    };*/
